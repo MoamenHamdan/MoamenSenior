080a5cd9a0203bc908505495fa23980cbf760acf
maomen hamdan <110568607+MoamenHamdan@users.noreply.github.com>
2025-04-30 01:34:29 +0300

    Add CurrencyRate and PhysicalLocation controllers with CRUD views
    - Implemented CurrencyRateController with actions for Index, Details, Create, Edit, and Delete.
    - Created corresponding views for CurrencyRate including Create, Edit, Details, Delete, and Index.
    - Implemented PhysicalLocationController with similar CRUD functionality and views.
    - Ensured proper model binding and validation in forms for both entities.
    - Enhanced user experience with user-friendly messages and structured layouts.

---
+++ M-Suite/Controllers/PhysicalLocationController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using M_Suite.Data;
+using M_Suite.Models;
+
+namespace M_Suite.Controllers
+{
+    public class PhysicalLocationController : Controller
+    {
+        private readonly MSuiteContext _context;
+
+        public PhysicalLocationController(MSuiteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PhysicalLocation
+        public async Task<IActionResult> Index()
+        {
+            var mSuiteContext = _context.PhysicalLocations.Include(p => p.PlBu).Include(p => p.PlCdIdPltNavigation).Include(p => p.PlMd).Include(p => p.PlPl);
+            return View(await mSuiteContext.ToListAsync());
+        }
+
+        // GET: PhysicalLocation/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var physicalLocation = await _context.PhysicalLocations
+                .Include(p => p.PlBu)
+                .Include(p => p.PlCdIdPltNavigation)
+                .Include(p => p.PlMd)
+                .Include(p => p.PlPl)
+                .FirstOrDefaultAsync(m => m.PlId == id);
+            if (physicalLocation == null)
+            {
+                return NotFound();
+            }
+
+            return View(physicalLocation);
+        }
+
+        // GET: PhysicalLocation/Create
+        public IActionResult Create()
+        {
+            ViewData["PlBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuId");
+            ViewData["PlCdIdPlt"] = new SelectList(_context.Codescs, "CdId", "CdId");
+            ViewData["PlMdId"] = new SelectList(_context.Modules, "MdId", "MdId");
+            ViewData["PlPlId"] = new SelectList(_context.PhysicalLocations, "PlId", "PlId");
+            return View();
+        }
+
+        // POST: PhysicalLocation/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PlId,PlPlId,PlCdIdPlt,PlMdId,PlLevel,PlCode,PlDescriptionLan1,PlDescriptionLan2,PlDescriptionLan3,PlBuId,PlActive,PlImpUid")] PhysicalLocation physicalLocation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(physicalLocation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuId", physicalLocation.PlBuId);
+            ViewData["PlCdIdPlt"] = new SelectList(_context.Codescs, "CdId", "CdId", physicalLocation.PlCdIdPlt);
+            ViewData["PlMdId"] = new SelectList(_context.Modules, "MdId", "MdId", physicalLocation.PlMdId);
+            ViewData["PlPlId"] = new SelectList(_context.PhysicalLocations, "PlId", "PlId", physicalLocation.PlPlId);
+            return View(physicalLocation);
+        }
+
+        // GET: PhysicalLocation/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var physicalLocation = await _context.PhysicalLocations.FindAsync(id);
+            if (physicalLocation == null)
+            {
+                return NotFound();
+            }
+            ViewData["PlBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuId", physicalLocation.PlBuId);
+            ViewData["PlCdIdPlt"] = new SelectList(_context.Codescs, "CdId", "CdId", physicalLocation.PlCdIdPlt);
+            ViewData["PlMdId"] = new SelectList(_context.Modules, "MdId", "MdId", physicalLocation.PlMdId);
+            ViewData["PlPlId"] = new SelectList(_context.PhysicalLocations, "PlId", "PlId", physicalLocation.PlPlId);
+            return View(physicalLocation);
+        }
+
+        // POST: PhysicalLocation/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PlId,PlPlId,PlCdIdPlt,PlMdId,PlLevel,PlCode,PlDescriptionLan1,PlDescriptionLan2,PlDescriptionLan3,PlBuId,PlActive,PlImpUid")] PhysicalLocation physicalLocation)
+        {
+            if (id != physicalLocation.PlId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(physicalLocation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PhysicalLocationExists(physicalLocation.PlId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PlBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuId", physicalLocation.PlBuId);
+            ViewData["PlCdIdPlt"] = new SelectList(_context.Codescs, "CdId", "CdId", physicalLocation.PlCdIdPlt);
+            ViewData["PlMdId"] = new SelectList(_context.Modules, "MdId", "MdId", physicalLocation.PlMdId);
+            ViewData["PlPlId"] = new SelectList(_context.PhysicalLocations, "PlId", "PlId", physicalLocation.PlPlId);
+            return View(physicalLocation);
+        }
+
+        // GET: PhysicalLocation/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var physicalLocation = await _context.PhysicalLocations
+                .Include(p => p.PlBu)
+                .Include(p => p.PlCdIdPltNavigation)
+                .Include(p => p.PlMd)
+                .Include(p => p.PlPl)
+                .FirstOrDefaultAsync(m => m.PlId == id);
+            if (physicalLocation == null)
+            {
+                return NotFound();
+            }
+
+            return View(physicalLocation);
+        }
+
+        // POST: PhysicalLocation/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var physicalLocation = await _context.PhysicalLocations.FindAsync(id);
+            if (physicalLocation != null)
+            {
+                _context.PhysicalLocations.Remove(physicalLocation);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PhysicalLocationExists(int id)
+        {
+            return _context.PhysicalLocations.Any(e => e.PlId == id);
+        }
+    }
+}
