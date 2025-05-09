7089e00eb5a0f0de5c670c6a071be4b4e3bfd36b
maomen hamdan <110568607+MoamenHamdan@users.noreply.github.com>
2025-05-09 16:58:57 +0300

    Add CRUD controllers and views for ItemWarehouse & Transaction

---
+++ M-Suite/Controllers/ItemWarehouseController.cs
@@ -0,0 +1,224 @@
+﻿// Updated ItemWarehouseController.cs
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using M_Suite.Data;
+using M_Suite.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M_Suite.Controllers
+{
+    public class ItemWarehouseController : Controller
+    {
+        private readonly MSuiteContext _context;
+
+        public ItemWarehouseController(MSuiteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ItemWarehouse
+        public async Task<IActionResult> Index()
+        {
+            var itemWarehouses = await _context.ItemWarehouses
+                .Include(i => i.ItwIt)
+                .Include(i => i.ItwPlIdWhsNavigation)
+                .Include(i => i.ItwUom)
+                .ToListAsync();
+
+            return View(itemWarehouses);
+        }
+
+        // GET: ItemWarehouse/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemWarehouse = await _context.ItemWarehouses
+                .Include(i => i.ItwIt)
+                .Include(i => i.ItwPlIdWhsNavigation)
+                .Include(i => i.ItwUom)
+                .FirstOrDefaultAsync(m => m.ItwId == id);
+
+            if (itemWarehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemWarehouse);
+        }
+
+        // GET: ItemWarehouse/Create
+        public IActionResult Create()
+        {
+            // Debugging: Check if reference tables have data
+            var hasItems = _context.Items.Any();
+            var hasLocations = _context.PhysicalLocations.Any();
+            var hasUoms = _context.Uoms.Any();
+
+            if (!hasItems || !hasLocations || !hasUoms)
+            {
+                TempData["Error"] = $"Missing data - Items: {hasItems}, Locations: {hasLocations}, UOMs: {hasUoms}";
+            }
+
+            PopulateDropdowns();
+            return View();
+        }
+
+        // POST: ItemWarehouse/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ItwPlIdWhs,ItwItId,ItwUomId,ItwQuantity,ItwQuantityDamage,ItwQuantityReserved,ItwQuantityPreviewed,ItwRemarks")] ItemWarehouse itemWarehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                itemWarehouse.ItwCreationDate = DateTime.UtcNow;
+
+
+                _context.Add(itemWarehouse);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            PopulateDropdowns(itemWarehouse);
+            return View(itemWarehouse);
+        }
+
+        // GET: ItemWarehouse/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemWarehouse = await _context.ItemWarehouses.FindAsync(id);
+            if (itemWarehouse == null)
+            {
+                return NotFound();
+            }
+
+            PopulateDropdowns(itemWarehouse);
+            return View(itemWarehouse);
+        }
+
+        // POST: ItemWarehouse/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ItwId,ItwPlIdWhs,ItwItId,ItwUomId,ItwQuantity,ItwQuantityDamage,ItwQuantityReserved,ItwQuantityPreviewed,ItwRemarks")] ItemWarehouse itemWarehouse)
+        {
+            if (id != itemWarehouse.ItwId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existingItem = await _context.ItemWarehouses.FindAsync(id);
+                    existingItem.ItwPlIdWhs = itemWarehouse.ItwPlIdWhs;
+                    existingItem.ItwItId = itemWarehouse.ItwItId;
+                    existingItem.ItwUomId = itemWarehouse.ItwUomId;
+                    existingItem.ItwQuantity = itemWarehouse.ItwQuantity;
+                    existingItem.ItwQuantityDamage = itemWarehouse.ItwQuantityDamage;
+                    existingItem.ItwQuantityReserved = itemWarehouse.ItwQuantityReserved;
+                    existingItem.ItwQuantityPreviewed = itemWarehouse.ItwQuantityPreviewed;
+                    existingItem.ItwRemarks = itemWarehouse.ItwRemarks;
+                    existingItem.ItwUpdateDate = DateTime.UtcNow;
+                    // existingItem.ItwUpdatedBy = User.Identity.IsAuthenticated ? int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value) : 0;
+
+                    _context.Update(existingItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ItemWarehouseExists(itemWarehouse.ItwId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            PopulateDropdowns(itemWarehouse);
+            return View(itemWarehouse);
+        }
+
+        // GET: ItemWarehouse/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var itemWarehouse = await _context.ItemWarehouses
+                .Include(i => i.ItwIt)
+                .Include(i => i.ItwPlIdWhsNavigation)
+                .Include(i => i.ItwUom)
+                .FirstOrDefaultAsync(m => m.ItwId == id);
+
+            if (itemWarehouse == null)
+            {
+                return NotFound();
+            }
+
+            return View(itemWarehouse);
+        }
+
+        // POST: ItemWarehouse/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var itemWarehouse = await _context.ItemWarehouses.FindAsync(id);
+            if (itemWarehouse != null)
+            {
+                _context.ItemWarehouses.Remove(itemWarehouse);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ItemWarehouseExists(int id)
+        {
+            return _context.ItemWarehouses.Any(e => e.ItwId == id);
+        }
+        private void PopulateDropdowns(ItemWarehouse itemWarehouse = null)
+        {
+            // Safely get lists or empty lists if null
+            var items = _context?.Items?.ToList() ?? new List<Item>();
+            var locations = _context?.PhysicalLocations?.ToList() ?? new List<PhysicalLocation>();
+            var uoms = _context?.Uoms?.ToList() ?? new List<Uom>();
+
+            ViewData["ItwItId"] = items.Any()
+                ? new SelectList(items, "ItId", "ItName", itemWarehouse?.ItwItId)
+                : new SelectList(new List<Item>(), "ItId", "ItName");
+
+            ViewData["ItwPlIdWhs"] = locations.Any()
+                ? new SelectList(locations, "PlId", "PlName", itemWarehouse?.ItwPlIdWhs)
+                : new SelectList(new List<PhysicalLocation>(), "PlId", "PlName");
+
+            ViewData["ItwUomId"] = uoms.Any()
+                ? new SelectList(uoms, "UomId", "UomName", itemWarehouse?.ItwUomId)
+                : new SelectList(new List<Uom>(), "UomId", "UomName");
+
+            // Add warning if no data
+            if (!items.Any() || !locations.Any() || !uoms.Any())
+            {
+                TempData["DropdownWarning"] = "Warning: Some dropdowns have no data. Please configure Items, Warehouses, and UOMs first.";
+            }
+        }
+    }
+}
---
+++ M-Suite/Controllers/TransactionController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using M_Suite.Data;
+using M_Suite.Models;
+
+namespace M_Suite.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly MSuiteContext _context;
+
+        public TransactionController(MSuiteContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Transaction
+        public async Task<IActionResult> Index()
+        {
+            var mSuiteContext = _context.Transactions.Include(t => t.TsBu).Include(t => t.TsCdIdCmsNavigation).Include(t => t.TsCdIdCurNavigation).Include(t => t.TsCdIdSrcNavigation).Include(t => t.TsSgd).Include(t => t.TsThpsIdBillNavigation).Include(t => t.TsThpsIdShipNavigation).Include(t => t.TsTss).Include(t => t.TsTst).Include(t => t.TsUs).Include(t => t.TsVt);
+            return View(await mSuiteContext.ToListAsync());
+        }
+
+        // GET: Transaction/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions
+                .Include(t => t.TsBu)
+                .Include(t => t.TsCdIdCmsNavigation)
+                .Include(t => t.TsCdIdCurNavigation)
+                .Include(t => t.TsCdIdSrcNavigation)
+                .Include(t => t.TsSgd)
+                .Include(t => t.TsThpsIdBillNavigation)
+                .Include(t => t.TsThpsIdShipNavigation)
+                .Include(t => t.TsTss)
+                .Include(t => t.TsTst)
+                .Include(t => t.TsUs)
+                .Include(t => t.TsVt)
+                .FirstOrDefaultAsync(m => m.TsId == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // GET: Transaction/Create
+        public IActionResult Create()
+        {
+            ViewData["TsBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuDescriptionLan1");
+            ViewData["TsCdIdCms"] = new SelectList(_context.Codescs, "CdId", "CdDescriptionLan1");
+            ViewData["TsCdIdCur"] = new SelectList(_context.Codescs, "CdId", "CdDescriptionLan1");
+            ViewData["TsCdIdSrc"] = new SelectList(_context.Codescs, "CdId", "CdDescriptionLan1");
+            ViewData["TsSgdId"] = new SelectList(_context.SignatureDetails, "SgdId", "SgdDescription");
+            ViewData["TsThpsIdBill"] = new SelectList(_context.Thirdparties, "ThpId", "ThpNameLan1");
+            ViewData["TsThpsIdShip"] = new SelectList(_context.Thirdparties, "ThpId", "ThpNameLan1");
+            ViewData["TsTssId"] = new SelectList(_context.Transactionstatuses, "TssId", "TssDescriptionLan1");
+            ViewData["TsTstId"] = new SelectList(_context.Transactiontypes, "TstId", "TstDescriptionLan1");
+            ViewData["TsUsId"] = new SelectList(_context.Users, "UsId", "UsFirstName");
+            ViewData["TsVtId"] = new SelectList(_context.Visits, "VtId", "VtOperation");
+            return View();
+        }
+
+        // POST: Transaction/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TsId,TsOrgId,TsUsId,TsBuId,TsVtId,TsTstId,TsTssId,TsCdIdSrc,TsCdIdCur,TsCdIdCms,TsThpsIdBill,TsThpsIdShip,TsSgdId,TsNumber,TsOurReference,TsTheirReference,TsDueDate,TsDate,TsDiscount,TsTotalDiscount,TsTotalDiscountBc,TsTotalTax,TsTotalTaxBc,TsTotal,TsTotalBc,TsRemarks,TsCreateDate,TsModifiedDate,TsCurRate,TsInvCurRate,TsUsIdCreatedby,TsPtId,TsAttribute01,TsAttribute02,TsAttribute03,TsDiscountAmount,TsUid,TsTotalFinal,TsExportedDate")] Transaction transaction)
+        {
+          
+                _context.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            
+            ViewData["TsBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuId", transaction.TsBuId);
+            ViewData["TsCdIdCms"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdCms);
+            ViewData["TsCdIdCur"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdCur);
+            ViewData["TsCdIdSrc"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdSrc);
+            ViewData["TsSgdId"] = new SelectList(_context.SignatureDetails, "SgdId", "SgdId", transaction.TsSgdId);
+            ViewData["TsThpsIdBill"] = new SelectList(_context.ThirdpartySites, "ThpsId", "ThpsId", transaction.TsThpsIdBill);
+            ViewData["TsThpsIdShip"] = new SelectList(_context.ThirdpartySites, "ThpsId", "ThpsId", transaction.TsThpsIdShip);
+            ViewData["TsTssId"] = new SelectList(_context.Transactionstatuses, "TssId", "TssId", transaction.TsTssId);
+            ViewData["TsTstId"] = new SelectList(_context.Transactiontypes, "TstId", "TstId", transaction.TsTstId);
+            ViewData["TsUsId"] = new SelectList(_context.Users, "UsId", "UsId", transaction.TsUsId);
+            ViewData["TsVtId"] = new SelectList(_context.Visits, "VtId", "VtId", transaction.TsVtId);
+            return View(transaction);
+        }
+
+        // GET: Transaction/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+            ViewData["TsBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuId", transaction.TsBuId);
+            ViewData["TsCdIdCms"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdCms);
+            ViewData["TsCdIdCur"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdCur);
+            ViewData["TsCdIdSrc"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdSrc);
+            ViewData["TsSgdId"] = new SelectList(_context.SignatureDetails, "SgdId", "SgdId", transaction.TsSgdId);
+            ViewData["TsThpsIdBill"] = new SelectList(_context.ThirdpartySites, "ThpsId", "ThpsId", transaction.TsThpsIdBill);
+            ViewData["TsThpsIdShip"] = new SelectList(_context.ThirdpartySites, "ThpsId", "ThpsId", transaction.TsThpsIdShip);
+            ViewData["TsTssId"] = new SelectList(_context.Transactionstatuses, "TssId", "TssId", transaction.TsTssId);
+            ViewData["TsTstId"] = new SelectList(_context.Transactiontypes, "TstId", "TstId", transaction.TsTstId);
+            ViewData["TsUsId"] = new SelectList(_context.Users, "UsId", "UsId", transaction.TsUsId);
+            ViewData["TsVtId"] = new SelectList(_context.Visits, "VtId", "VtId", transaction.TsVtId);
+            return View(transaction);
+        }
+
+        // POST: Transaction/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TsId,TsOrgId,TsUsId,TsBuId,TsVtId,TsTstId,TsTssId,TsCdIdSrc,TsCdIdCur,TsCdIdCms,TsThpsIdBill,TsThpsIdShip,TsSgdId,TsNumber,TsOurReference,TsTheirReference,TsDueDate,TsDate,TsDiscount,TsTotalDiscount,TsTotalDiscountBc,TsTotalTax,TsTotalTaxBc,TsTotal,TsTotalBc,TsRemarks,TsCreateDate,TsModifiedDate,TsCurRate,TsInvCurRate,TsUsIdCreatedby,TsPtId,TsAttribute01,TsAttribute02,TsAttribute03,TsDiscountAmount,TsUid,TsTotalFinal,TsExportedDate")] Transaction transaction)
+        {
+            if (id != transaction.TsId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(transaction);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TransactionExists(transaction.TsId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TsBuId"] = new SelectList(_context.BusinessUnits, "BuId", "BuId", transaction.TsBuId);
+            ViewData["TsCdIdCms"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdCms);
+            ViewData["TsCdIdCur"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdCur);
+            ViewData["TsCdIdSrc"] = new SelectList(_context.Codescs, "CdId", "CdId", transaction.TsCdIdSrc);
+            ViewData["TsSgdId"] = new SelectList(_context.SignatureDetails, "SgdId", "SgdId", transaction.TsSgdId);
+            ViewData["TsThpsIdBill"] = new SelectList(_context.ThirdpartySites, "ThpsId", "ThpsId", transaction.TsThpsIdBill);
+            ViewData["TsThpsIdShip"] = new SelectList(_context.ThirdpartySites, "ThpsId", "ThpsId", transaction.TsThpsIdShip);
+            ViewData["TsTssId"] = new SelectList(_context.Transactionstatuses, "TssId", "TssId", transaction.TsTssId);
+            ViewData["TsTstId"] = new SelectList(_context.Transactiontypes, "TstId", "TstId", transaction.TsTstId);
+            ViewData["TsUsId"] = new SelectList(_context.Users, "UsId", "UsId", transaction.TsUsId);
+            ViewData["TsVtId"] = new SelectList(_context.Visits, "VtId", "VtId", transaction.TsVtId);
+            return View(transaction);
+        }
+
+        // GET: Transaction/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var transaction = await _context.Transactions
+                .Include(t => t.TsBu)
+                .Include(t => t.TsCdIdCmsNavigation)
+                .Include(t => t.TsCdIdCurNavigation)
+                .Include(t => t.TsCdIdSrcNavigation)
+                .Include(t => t.TsSgd)
+                .Include(t => t.TsThpsIdBillNavigation)
+                .Include(t => t.TsThpsIdShipNavigation)
+                .Include(t => t.TsTss)
+                .Include(t => t.TsTst)
+                .Include(t => t.TsUs)
+                .Include(t => t.TsVt)
+                .FirstOrDefaultAsync(m => m.TsId == id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return View(transaction);
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TransactionExists(int id)
+        {
+            return _context.Transactions.Any(e => e.TsId == id);
+        }
+    }
+}
